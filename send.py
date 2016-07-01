4bd09aedfd6f76b3ef948c25aaa4b9f8a7d45b55
ahmedakef <aemed.akef.1@gmail.com>
2016-07-01 14:30:47 +0200

    make loop in send.py

--- send.py
+++ send.py
@@ -4,7 +4,8 @@ def send_data(data):
     f.write(data)
     f.close()
     os.system("scp data.txt pi@196.168.0.111:") # change pi to the remote user name
-                                               #and the ip to your remote user nme
-
-data = raw_input("enter what to send : ")
-send_data(data)
+                                                #and the ip to your remote user nme
+while True:
+    data = raw_input("\nEnter what to send : ")
+    send_data(data)
+    if raw_input("Ener 0 to exit and any key to continue : ")=='0':break
